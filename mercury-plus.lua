a6dd926450788f6b36a64bc232c717ed2f325618
Moonzy <106113415+SpyTYX@users.noreply.github.com>
2023-02-02 22:54:18 +0000

    Update mercury-plus.lua

--- mercury-plus.lua
+++ mercury-plus.lua
@@ -2027,7 +2027,7 @@ function Library:button(options)
 	return methods
 end
 
-function Library:testing(options)
+function Library:label(options)
 	options = self:set_defaults({
 		Name = "test",
 		Description = nil,
@@ -2058,6 +2058,16 @@ function Library:testing(options)
 	end
 	do
 	end
+	self:_resize_tab()
+end
+
+function Library:line(options)
+	options = self:set_defaults({
+	}, options)
+	local lineContainer = self.container:object("TextLabel", {
+		Theme = {BackgroundColor3 = "Secondary"},
+		Size = UDim2.new(1, -1, 0, 52)
+	}):round(7)
 	self:_resize_tab()
 end
 
@@ -3708,58 +3718,3 @@ function Library:textbox(options)
 
 	return methods
 end
-
-function Library:label(options)
-
-	options = self:set_defaults({
-		Text = "Label title",
-		Description = "Label text",
-	}, options)
-
-	local labelContainer = self.container:object("TextButton", {
-		Theme = {BackgroundColor3 = "Secondary"},
-		Size = UDim2.new(1, -20, 0, 52),
-		BackgroundTransparency = 1
-	}):round(7):stroke("Secondary", 2)
-
-	local text = labelContainer:object("TextLabel", {
-		BackgroundTransparency = 1,
-		Position = UDim2.fromOffset(10, 5),
-		Size = UDim2.new(0.5, -10, 0, 22),
-		Text = options.Text,
-		TextSize = 22,
-		Theme = {TextColor3 = "StrongText"},
-		TextXAlignment = Enum.TextXAlignment.Left
-	})
-
-	local description = labelContainer:object("TextLabel", {
-		BackgroundTransparency = 1,
-		Position = UDim2.new(0, 10, 1, -5),
-		Size = UDim2.new(0.5, -10, 1, -22),
-		Text = options.Description,
-		TextSize = 18,
-		AnchorPoint = Vector2.new(0, 1),
-		Theme = {TextColor3 = "WeakText"},
-		TextXAlignment = Enum.TextXAlignment.Left
-	})
-
-	self:_resize_tab()
-
-	local methods = {}
-
-	function methods:SetText(txt)
-		text.Text = txt
-	end
-
-	function methods:SetDescription(txt)
-		description.Text = txt
-	end
-
-	return methods
-end
-
-return setmetatable(Library, {
-	__index = function(_, i)
-		return rawget(Library, i:lower())
-	end
-})
